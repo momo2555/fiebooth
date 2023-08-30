705e04d9ade4e899769d7f8177c62641d9e4673f
Mohamed Al Glawi <shadowrapplique@gmail.com>
2023-08-30 23:54:03 +0200

    add python version setup env script

---
+++ setup_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install ./drivers/drivers/ql800pdrv-2.1.4-0.armhf.deb
+
+curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+pyenv install 3.9.2
