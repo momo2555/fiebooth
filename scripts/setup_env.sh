1e2d7120d4419dcdcf7901c5a64506fdef8476e9
Mohamed AL Glawi <mohamed.alglawi@gmail.com>
2023-10-14 13:21:57 +0200

    setup env tested (to be continued)

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -7,38 +7,49 @@
 #echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 #source ~/.bashrc
 #pyenv install 3.9.2
-pushd ..
+#pushd ..
 # python env install
-python -m pip install requirements.txt
+#echo "Update python environment ..."
+#python -m pip install requirements.txt
+
+#install fiebooth portail
+#echo "Install Fiebooth Portail ..."
+#bash scripts/install_portail.sh
 
 # install camera
-wget -O install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
-chmod +x install_pivariety_pkgs.sh
-./install_pivariety_pkgs.sh -p libcamera_dev
-./install_pivariety_pkgs.sh -p libcamera_apps
-./install_pivariety_pkgs.sh -p 64mp_pi_hawk_eye_kernel_driver
+#echo "Install camera drivers ..."
+#wget -O install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
+#chmod +x install_pivariety_pkgs.sh
+#./install_pivariety_pkgs.sh -p libcamera_dev
+#./install_pivariety_pkgs.sh -p libcamera_apps
+#./install_pivariety_pkgs.sh -p 64mp_pi_hawk_eye_kernel_driver
 # dtoverlay=vc4-kms-v3d,cma-512 > /boot/config.txt [pi4]
 
 # configure printer
-sudo usermod -a -G lpadmin fiebooth
-sudo usermod -a -G lp fiebooth
+#echo "Configure printer ..."
+#sudo usermod -a -G lpadmin fiebooth
+#sudo usermod -a -G lp fiebooth
 
 # install infra
-sudo apt install -y hostapd dnsmasq
-sudp cp scrpts/files/hostapd.conf /etc/hostapd/hostapd.conf
-sudo echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> /etc/default/hostapd #fin du fichier
+echo "Install infra, setup DNS and wifi Accsess Point ..."
+#sudo apt install -y hostapd dnsmasq
+ls
+sudo cp scripts/files/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp scripts/files/hostapd /etc/default/hostapd #fin du fichier
 sudo systemctl unmask hostapd
 sudo systemctl enable hostapd
 sudo cp scripts/files/dnsmasq.conf /etc/dnsmasq.conf
-sudo echo "192.168.42.10 portail.fiebooth" >> sudo nano /etc/hosts # fin du fichier
+sudo cp scripts/files/hosts /etc/hosts # fin du fichier
 sudo cp scripts/files/dhcpcd-ap.conf /etc/dhcpcd.conf
 
 # auto run on fiebooh :
-cp scripts/files/fiebooth.service etc/systemd/system/fiebooth.service
+echo "Setup systemd service for Fiebooth ..."
+sudo cp scripts/files/fiebooth.service /etc/systemd/system/fiebooth.service
 sudo systemctl daemon-reload
 sudo systemctl enable fiebooth.service
 
 # reboot raspberry pi
-sudo reboot
-popd 
+echo "Reboot Fiebooth ..."
+#sudo reboot
+#popd 
 
