bfd3e7a827dca724c6cc38dce79ab11afd571c57
Mohamed AL Glawi <mohamed.alglawi@gmail.com>
2023-10-14 13:21:27 +0200

    update wifi config

--- scripts/wifi_mode.sh
+++ scripts/wifi_mode.sh
@@ -13,10 +13,11 @@ sudo systemctl stop dnsmasq
 sudo systemctl stop hostapd
 
 # reconfigure wifi
-cp files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed "/{ssid}/$SSID/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed "/{psk}/$psk/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/_ssid_/$SSID/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sudo sed -i -e "s/_psk_/$PSK/g" /etc/wpa_supplicant/wpa_supplicant.conf
 
 # restart the dhcp with wifi configuration
 sudo cp files/dhcpcd-wifi.conf /etc/dhcpcd.conf
 sudo service dhcpcd restart
+
