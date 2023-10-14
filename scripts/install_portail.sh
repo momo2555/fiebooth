7f0a8cd41440a20bb680d464c9959ad7301181fb
Mohamed AL Glawi <mohamed.alglawi@gmail.com>
2023-10-14 13:20:54 +0200

    update portail version

--- scripts/install_portail.sh
+++ scripts/install_portail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O portail.zip https://github.com/momo2555/fiebooth_portail/releases/download/FIEBOOTH_PORTAIL_0.1/fiebooth_portail_0.1.zip
+wget -O portail.zip https://github.com/momo2555/fiebooth_portail/releases/download/FIEBOOTH_PORTAIL_0.2/fiebooth_portail_0.2.zip
 unzip portail.zip
 if [ -d "portail" ]; then
     rm -rf portail
