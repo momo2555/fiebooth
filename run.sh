4ab57fc441da8dada5f591b91980678f1fda4b3d
Mohamed Al Glawi <mohamed.alglawi@gmail.com>
2023-10-15 18:19:15 +0200

    update run script

--- run.sh
+++ run.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 # Run Fiebooth 
+cd /fiebooth/fiebooth
 cd src
-DISPLAY=:0 python main.py &
+source ~/.bashrc
+export "XAUTHORITY=$HOME/.Xauthority"
+export "PYTHONPATH=$PYTHONPATH:$HOME/.pyenv/versions/fiebooth/lib/python3.9/site-packages"
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+DISPLAY=:0 XAUTHORITY=$XAUTHORITY  /home/fiebooth/.pyenv/versions/fiebooth/bin/python main.py &
 cd ..
 
 # Run Portail
 cd portail
 sudo python -m http.server 80 &
 cd ..
+
+pids=$(jobs -p)
+kill_processes() {
+  for pid in $pids; do
+    kill $pid
+  done
+}
+trap 'kill_processes' SIGINT
 
 wait
