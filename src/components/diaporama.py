edab6f35e9045026f7596f15635c5da4a0b35325
Mohamed Al Glawi <mohamed.alglawi@gmail.com>
2023-10-21 11:58:26 +0200

    first version of dynamic diaporama (not added)

--- src/components/diaporama.py
+++ src/components/diaporama.py
@@ -1,42 +1,74 @@
 from .component_base import ComponentBase
 from utils.image_utils import ImageUtils
+from utils.win_utils import WinUtils
 from config import config
 import random as rd
+import pygame
+import os
+
 
 class Diaporama(ComponentBase):
-    def __init__(self, window_context, w, h, x):
+    MAX_FRAME = 3
+
+    def __init__(self, window_context, w, h, x, speed : int = 7):
         super().__init__(window_context)
         self.__w = w
-        self.__h = h 
+        self.__h = h
         self.__x = x
         self.__images = []
-    
+        self.__speed = speed
+
+
     def pick_images(self):
-        self.__photos : dict = ImageUtils.get_all_user_photos_path(config.user_name)
-        self.__possibilities : dict = self.__photos.copy()
+        self.__photos: dict = ImageUtils.get_all_user_photos_path(config.user_name)
+        self.__possibilities: dict = self.__photos.copy()
 
         for im in self.__images:
             if im["path"] in self.__photos:
                 self.__possibilities.remove(im)
         length = len(self.__possibilities)
-        # delete the last image
-         
-        # add new image
-        while len(self.__images) < 3:
-            self.__images.insert(0, {
-                "path" : self.__possibilities[rd.randint(0, length -1)],
-                "pos" : [self.__x, ]
-            })
 
-    def __is_outside_bounds(self, img_obj):
-        pass
+        # delete the last image
+        if self.__img_len() > 0:
+            self.__images.pop()
+
+        # add new image
+        while self.__img_len() <= self.MAX_FRAME:
+            error = False
+            current_photo = self.__possibilities[rd.randint(0, length - 1)]
+            try:
+                if os.path.exists(current_photo):
+                    img = pygame.image.load(current_photo).convert()
+                else:
+                    error = True
+            except pygame.error as e:
+                self._logger.warning(
+                    f"Image format not supported : {current_photo}"
+                )
+                error = True
+            if not error:
+                y = -self.__h + int((self.MAX_FRAME - self.__img_len()) * self.__h * 1.08)
+                self.__images.insert(
+                0,
+                {
+                    "path": current_photo,
+                    "pos": [self.__x, y],
+                    "data": pygame.transform.scale(img, (self.__w, self.__h)),
+                },
+                )
+            self.__possibilities.remove(current_photo)
+
+    def __img_len(self) -> int:
+        return len(self.__images)
+    
+    def __is_outside_bounds(self, img_obj) -> bool:
+        return img_obj["pos"][1] > WinUtils.hprct(1)
 
     def setup(self):
         for im in self.__images:
-            pos = (self.__photo_pos[0], self.__photo_pos[1] - WinUtils.hprct(0.5))
-            self._window.blit(self.__img, pos)
-            self.__photo_pos[1] = (self.__photo_pos[1]+self.__speed)%WinUtils.hprct(1.5)
-            if self.__photo_pos[1] < self.__speed+1:
-                self.__choose_photo()
-
-
+            pos = (im["pos"][0], im["pos"][1])
+            self._window.blit(im["data"], pos)
+            im["pos"][1] = (im["pos"][1] + self.__speed)
+        
+        if self.__is_outside_bounds(self.__images[-1]):
+            self.pick_images()
